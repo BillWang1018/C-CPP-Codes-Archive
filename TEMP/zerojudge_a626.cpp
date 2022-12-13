5e3af33039ca15d1fe0e6149fde4a7b987f3bad9
Bill <billwang1018@gmail.com>
2022-12-13 23:37:03 +0800

    .

---
+++ TEMP/zerojudge_a626.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main() {
+    int N{}, countPrint=1;
+    cin >> N;
+
+    if (N >= 2)
+        cout << setw(10) << 2;
+    if (N <= 2)
+        return 0;
+    
+    vector<int> primes;
+    primes.push_back(2);
+    bool flag;
+    int lastPrime = 2;
+    for(int i=3; i<=N; i++) {
+        flag = true;
+        for(auto p:primes) {
+            if(!(i%p)) {
+                flag = false;
+                break;
+            }
+        }
+        if(!flag) continue; 
+        for(int j = lastPrime; j<sqrt(i); j++) {
+            if(!(i%j)) {
+                flag = false;
+                break;
+            }
+        }
+
+        if(flag) {
+            lastPrime = i;
+            primes.push_back(i);
+            cout << setw(10) << i;
+            countPrint++;
+            if(countPrint >= 5) {
+                cout << '\n';
+                countPrint = 0;
+            }
+        }
+    }
+    cout << endl;
+    return 0;
+}
