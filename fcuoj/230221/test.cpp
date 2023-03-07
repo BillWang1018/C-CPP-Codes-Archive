7021691bab05629a33e1a49cbcd4e83d53ee8f8f
Bill <billwang1018@gmail.com>
2023-03-07 10:52:38 +0800

    idk what's these

--- fcuoj/230221/test.cpp
+++ fcuoj/230221/test.cpp
@@ -1,17 +1,31 @@
-#include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
-int main() {
-    int num[] = {0,1,2};
-    cout << num << '\n';
-    int *l = num, *r = num+3-1;
-    while(l < r) { // cool bitwise xor thing to swap(l,r)
-        *l ^= *r;
-        *r ^= *l;
-        *l ^= *r;
-        
-        l++, r--;
+
+int main()
+{
+    int n;
+    string line;
+    int count[91] = {0};
+    cin >> n;
+    getchar();
+    while(n--)
+    {
+        getline(cin, line);
+        for(int i=0 ; i<line.size() ; i++)
+        {
+            if(line[i]>='a' && line[i]<='z')
+                count[line[i]-'a'+'A']++;
+            else
+                count[line[i]]++;
+        }
     }
-    cout << num << '\n';
-    cout << num+1 << '\n';
-    cout << num[2] << '\n';
+    for(int i=sizeof(line) ; i>=1 ; i--)
+    {
+        for(int j='A'; j<='Z'; j++)
+        {
+            if(count[j] == i)
+                cout << char(j) << " " << i << endl;
+        }
+    }
+    return 0;
 }
