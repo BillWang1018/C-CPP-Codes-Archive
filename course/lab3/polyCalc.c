c960b00968a895631f8c5f08eed7db55b4c3a78c
Bill <billwang1018@gmail.com>
2023-10-08 16:55:41 +0800

    update comments

--- course/lab3/polyCalc.c
+++ course/lab3/polyCalc.c
@@ -9,37 +9,52 @@
 #define co coefficient
 #define ex exponent
 #define altoi(A) ((A)-'A')  // alpha to index
-#define MAX_POLYS 30
-// #define MAX_TERMS 10        // max terms of each polynominal can hold
+#define MAX_POLYS 30        // (A-Z)
 
 typedef struct Polynominal {
     double coefficient;
     int exponent;
-    struct Polynominal *nterm;
+    struct Polynominal *nterm; // linked list
 } Poly;
 
+/////////////////////
+/* other functions */
+/////////////////////
+// Delete whole list and *head from poly[]
 void delList_inArr(char);
+// Delete whole list from pointer given
 void delList(Poly**);
+// Push back to the poly[], create one if none
 void pushList_inArr(char, double, int);
+// Push back to the pointer (list) given, create one if none
 void push(Poly**, double, int);
-char *substr();
+// Return a substring with a len of n
+char *substr(char*, int);
+// Print and format Poly for the given pointer (list)
 void printfPoly(Poly *);
 
+//////////////////
+/* HW functions */
+//////////////////
+// Enter a poly using format A=... and store it
 void readPoly();
+// Enter a poly and print it out
 void printPoly();
+// Enter two polys and add them 
 void Padd();
+// Enter two polys and multiply them
 void Pmult();
+// Enter a number and then a poly to caculate the result
 void Peval();
 
-// final element in array is reserved to record terms count
-// polys_array[][MAX_TERMS].ex == terms count 
-// Poly polys_array[MAX_POLYS][MAX_TERMS+1];
-// using list
+// Globle variables 
 Poly *polyHead[MAX_POLYS]={0};
 Poly *polyBack[MAX_POLYS]={0};
 
+
 int main() {
 
+    // reads 1-6 for different functionality
     char cmd;
     while(scanf("%c", &cmd)) {
         switch (cmd) {
@@ -58,11 +73,10 @@ int main() {
         case '5':
             Peval();
             break;
-        
-        default:
+        case '6':
             return 0;
         }
-
+        // flush unwanted input
         fflush(stdin);
     }
     return 0;
@@ -72,19 +86,24 @@ int main() {
 
 void delList_inArr(char i) {
     Poly *cur = polyHead[i], *next;
+    // return if the list is empty
     if(cur == NULL) return;
+    // free all memories
     while(cur != NULL) {
         next = cur->nterm;
         free(cur);
         cur = next;
     }
+    // clear heads and tails
     polyHead[i] = NULL;
     polyBack[i] = NULL;
 }
 
 void delList(Poly **p) {
-    if((*p) == NULL) return;
     Poly *cur = *p, *next;
+    // return if the list is empty
+    if(cur == NULL) return;
+    // free all memories
     while(cur != NULL) {
         next = cur->nterm;
         free(cur);
@@ -94,15 +113,17 @@ void delList(Poly **p) {
 }
 
 void pushList_inArr(char i, double co, int ex) {
+    // create a new node 
     Poly *node = (Poly *)malloc(sizeof(Poly));
     node->co = co;
     node->ex = ex;
     node->nterm = NULL;
 
+    // if there is no node in the list
     if(polyBack[i] == NULL) {
         polyBack[i] = node;
         polyHead[i] = node;
-    } else {
+    } else { // append list
         polyBack[i]->nterm = node;
         polyBack[i] = node;
     }
@@ -110,16 +131,18 @@ void pushList_inArr(char i, double co, int ex) {
 }
 
 void push(Poly **head, double co, int ex) {
+    // create a new node
     Poly *node = (Poly *)malloc(sizeof(Poly));
     node->co = co;
     node->ex = ex;
     node->nterm = NULL;
 
-
+    // if there is no node in the list
     if(*head == NULL) {
         *head = node;
-    } else {
+    } else { // append list
         Poly *tmp = *head;
+        // find the last element in the list
         while(tmp->nterm != NULL) {
             tmp = tmp->nterm;
         }
@@ -137,30 +160,32 @@ char *substr(char* str, int n) {
 }
 
 void printfPoly(Poly *head) {
+    // do not need to add plus sign if the first term is positive
     char firstTerm = 1;
     Poly *tmp = head;
+    // iterate until end of list
     while(tmp != NULL) {
-
+        // if coe. is positive
         if(tmp->co >= 0 && !firstTerm)
             printf("+");
         printf("%.1lf", tmp->co);
+        // if not constant (exp. > 0)
         if(tmp->ex != 0)
             printf("x");
+        // if exp. > 1
         if(tmp->ex != 0 && tmp->ex != 1)
             printf("^%d", tmp->ex);
 
         tmp = tmp->nterm;
-
         firstTerm = 0;
-
     }
 
     printf("\n");
-
-    free(tmp);
 }
 
-/* POLYNOMINALS METHODS START FROM HERE */
+////////////////////////////////
+/* HW METHODS START FROM HERE */
+////////////////////////////////
 
 void readPoly() {
     char input[100];
@@ -172,7 +197,8 @@ void readPoly() {
     scanf("%s", input);
 
     iptr = input;
-
+    
+    // check if first char. is alphabet 
     if(!isalpha(*iptr)) {
         printf("erro1 at - %c\n", *iptr);
         readPoly();
@@ -181,6 +207,7 @@ void readPoly() {
 
     iptr++;
 
+    // check if second char. is equal sign
     if(*iptr != '=') {
         printf("erro2 at - %c\n", *iptr);
         readPoly();
@@ -188,7 +215,6 @@ void readPoly() {
     }
 
     iptr++;
-    
 
     /* PROCESS polynominal */
 
@@ -196,26 +222,26 @@ void readPoly() {
     double dco=0;
     int iex=0;
     int len=0;
-    // int term=0;
     alpha = altoi(alpha);
     delList_inArr(alpha);
 
     while(*iptr != '\0') {
         /* PROCESS coefficient */
         next = iptr;
-        len=0;
-        // FIND X
+        len=0; // count the length of numbers
+        // find next "x"
         while(1) {
+            // no "x", rest of the numbers is constant (x^0)
             if(*next == '\0') {
-                // no more x, CONSTANT only
                 dco = atof(substr(iptr, len));
                 break;
             }
+            // found "x"
             if(*next == 'x') {
-                // check if term is -x or +x or x
+                // check if term is -x or +x or x (coe. is 1 or -1)
                 if((*(next-1) == '-' || *(next-1) == '+') || *(next-1) == '=')
                     dco = (*(next-1) == '-') ? -1 : 1;
-                else // n*x
+                else // coe. is numbers before x
                     dco = atof(substr(iptr, len));
                 next++; // points to next term
                 break;
@@ -225,18 +251,18 @@ void readPoly() {
         
         iptr = next;
         /* PROCESS exponent */
-        // CONSTANT only
+        // is constant
         if(*iptr == '\0' && *(iptr-1) != 'x') {
             iex = 0;
         }
-        // FIND ^
+        // find next "^"
         else if(*iptr == '^') {
             iptr++;
             next = iptr;
             len = 0;
             while(1) {
+                // end of a term
                 if(*next == '\0' || (*next == '+' || *next == '-')) {
-                    // end of a term
                     iex = atoi(substr(iptr, len));
                     break;
                 }
@@ -244,10 +270,11 @@ void readPoly() {
             }
             iptr = next;
         } 
-        // NO ^ (x^1)
+        // no "^" (x^1)
         else if(*iptr == '\0' || (*iptr == '+' || *iptr == '-')) {
             iex = 1;
         }
+        // the input is not illegal 
         else {
             printf("erro3 at - %c", *iptr);
             readPoly();
@@ -256,14 +283,8 @@ void readPoly() {
         
         // store data
         pushList_inArr(alpha, dco, iex);
-        // polys_array[alpha][term].co = dco;
-        // polys_array[alpha][term].ex = iex;
-        // term++;
 
     }
-
-    // polys_array[alpha][MAX_TERMS].ex = term;
-
 }
 
 void printPoly() {
@@ -275,6 +296,7 @@ void printPoly() {
     scanf("%c", &c);
     c = altoi(c);
     node = polyHead[c];
+    // return if the list is empty
     if(node == NULL) {
         printf("Not exist.\n");
         return;
@@ -291,11 +313,14 @@ void Padd() {
     c1 = altoi(c1), c2 = altoi(c2);
     Poly *ans = NULL, **ansHead = &ans;
     Poly *p1 = polyHead[c1], *p2 = polyHead[c2];
+    // return if any of two poly is empty
     if(p1 == NULL || p2 == NULL) {
         printf("The variable(s) is empty!\n");
         return;
     }
+    // iterate p1 & p2
     while(p1 != NULL || p2 != NULL) {
+        // if one of two poly has reach the end
         if(p1 == NULL) {
             push(&ans, p2->co, p2->ex);
             p2 = p2->nterm;
@@ -306,15 +331,15 @@ void Padd() {
             p1 = p1->nterm;
             continue;
         }
-
+        // compare the exp. of two polys
         if(p1->ex > p2->ex) {
             push(&ans, p1->co, p1->ex);
             p1 = p1->nterm;
-        } else
+        }
         if(p1->ex < p2->ex) {
             push(&ans, p2->co, p2->ex);
             p2 = p2->nterm;
-        } else
+        }
         if(p1->ex == p2->ex) {
             push(&ans, (p1->co + p2->co), p1->ex);
             p1 = p1->nterm;
@@ -335,6 +360,7 @@ void Pmult() {
     c1 = altoi(c1), c2 = altoi(c2);
     Poly *ans = NULL, **ansHead = &ans;
     Poly *p1 = polyHead[c1], *p2 = polyHead[c2];
+    // return if any of two poly is empty
     if(p1 == NULL || p2 == NULL) {
         printf("The variable(s) is empty!\n");
         return;
@@ -344,7 +370,9 @@ void Pmult() {
     int ex;
     char notCreate=1;
 
+    // iterate p1
     while(p1 != NULL) {
+        // iterate p2
         while(p2 != NULL) {
             co = p1->co * p2->co;
             ex = p1->ex + p2->ex;
@@ -377,10 +405,8 @@ void Pmult() {
                     (tmp)->nterm = newp;
                     break;
                 }
-
                 tmp = tmp->nterm;
             }
-
             p2 = p2->nterm;
         }
         p1 = p1->nterm;
@@ -406,13 +432,11 @@ void Peval() {
     }
 
     printf("%.1lf\n", ans);
-
-
 }
 
 
 
-/* test cases
+/* Test cases
 
 1 
 A=x
